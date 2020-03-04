9f44b32ae6278ccd9ddf36cbe2a043b3e2b8b5e7
Juan C <juanceballos@pursuit.org>
2020-03-03 20:34:40 -0500

    set up pods

---
+++ FireBase-Auth-Lab/FireBase-Auth-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBase-Auth-Lab
+//
+//  Created by Juan Ceballos on 3/3/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+}
+
---
+++ FireBase-Auth-Lab/Pods/Target Support Files/Pods-FireBase-Auth-Lab/Pods-FireBase-Auth-Lab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireBase_Auth_Lab : NSObject
+@end
+@implementation PodsDummy_Pods_FireBase_Auth_Lab
+@end
