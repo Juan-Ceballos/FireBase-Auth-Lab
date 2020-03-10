89dab53c9f24597f5c92a1a11b372a53618fc95d
Juan C <juanceballos@pursuit.org>
2020-03-10 11:52:17 -0400

    editable fields user profile

--- FireBase-Auth-Lab/FireBase-Auth-Lab/Controllers/ProfileViewController.swift
+++ FireBase-Auth-Lab/FireBase-Auth-Lab/Controllers/ProfileViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
+    
+    //textField.text
     
     private let profileView = ProfileView()
     
@@ -19,7 +22,75 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemOrange
+       updateUI()
+        profileView.updateProfileButton.addTarget(self, action: #selector(updateProfileButtonPressed), for: .touchUpInside)
+        
+        // 50ish mins firebase 1 store
+        //user.displayName
+        //user.email
+        //user.photoURL
+        
+        //user.delete { () in
+        //
+        //}
+        
+        
+        //user.phoneNumber
+        //user.uid
+        //user.updateEmail(to: , completion: )
+    }
+    
+    @objc private func updateProfileButtonPressed()    {
+        guard let displayName = profileView.userNameTextField.text,
+            let currentEmail = profileView.emailTextField.text,
+            let phoneNumber = profileView.phoneNumberTextField.text,
+            !displayName.isEmpty && !currentEmail.isEmpty && !phoneNumber.isEmpty
+            else    {
+                print("missing fields")
+                return
+        }
+        
+        let request = Auth.auth().currentUser?.createProfileChangeRequest()
+        request?.displayName = displayName
+        request?.commitChanges(completion: { (error) in
+            if let error = error    {
+                print("error userName")
+            }
+            else    {
+                print("success userName")
+            }
+        })
+        
+        // re-authenticate user if logged in long time ago
+        Auth.auth().currentUser?.updateEmail(to: currentEmail, completion: { (error) in
+            if let error = error    {
+                print("\(error)")
+            }
+            else    {
+                print("success email")
+            }
+        })
+        
+        // phone number
+        
+        // need storage for user profile pic
+        
+        // delete also requires reautthentication like updateEmail()
+    }
+    
+    private func updateUI() {
+        guard let user = Auth.auth().currentUser
+        else    {
+            return
+        }
+        
+        profileView.userNameTextField.text = user.displayName
+        profileView.emailTextField.text = user.email
         
     }
-
+    
+    //updateProfileButtonPressed
+    //user.createProfileChangeRequest()
+    //request.displayName
+    //request.commitChanges
 }
--- FireBase-Auth-Lab/FireBase-Auth-Lab/Views/ProfileView.swift
+++ FireBase-Auth-Lab/FireBase-Auth-Lab/Views/ProfileView.swift
@@ -10,24 +10,49 @@ import UIKit
 import SnapKit
 
 class ProfileView: UIView {
+      
+    private lazy var profileStackView: UIStackView =    {
+        let stackView = UIStackView()
         
-    private lazy var emailLabel: UILabel =  {
-        let label = UILabel()
-        return label
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        stackView.addArrangedSubview(userNameTextField)
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(phoneNumberTextField)
+        stackView.addArrangedSubview(updateProfileButton)
+        
+        return stackView
     }()
     
-    private lazy var phoneNumberLabel: UILabel =    {
-        let label = UILabel()
-        return label
+    public lazy var updateProfileButton: UIButton =    {
+        let button = UIButton()
+        button.setTitle("Update Profile", for: .normal)
+        return button
     }()
     
-    private lazy var userNameLabel: UILabel =   {
-        let label = UILabel()
-        return label
+    public lazy var emailTextField: UITextField =  {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        return textField
     }()
     
-    private lazy var profileImageImageView: UIImageView =   {
+    public lazy var phoneNumberTextField: UITextField =    {
+        let textField = UITextField()
+        textField.placeholder = "Phone Number"
+        return textField
+    }()
+    
+    public lazy var userNameTextField: UITextField =   {
+        let textField = UITextField()
+        textField.placeholder = "UserName"
+        return textField
+    }()
+    
+    public lazy var profileImageImageView: UIImageView =   {
         let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person")
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -43,7 +68,30 @@ class ProfileView: UIView {
     }
     
     private func commonInit()   {
+        setupProfileImageViewConstraints()
+        setupProfileStackViewConstraints()
+    }
+    
+    private func setupProfileImageViewConstraints() {
+        addSubview(profileImageImageView)
         
+        profileImageImageView.snp.makeConstraints { (make) in
+            make.left.equalTo(self)
+            make.right.equalTo(self)
+            make.height.equalTo(self).multipliedBy(0.2)
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+        }
+    }
+    
+    private func setupProfileStackViewConstraints() {
+        addSubview(profileStackView)
+        
+        profileStackView.snp.makeConstraints { (make) in
+            make.top.equalTo(profileImageImageView.snp.bottom).offset(22)
+            make.left.equalTo(self)
+            make.right.equalTo(self)
+            make.height.equalTo(self).multipliedBy(0.2)
+        }
     }
     
 }
